3727bd6c0b6e408a089b17671797427b19069ed3
Sokratis Vidros <SokratisVidros@users.noreply.github.com>
2026-03-28 08:42:39 +0200

    Update migration.ts

--- src/db/migration.ts
+++ src/db/migration.ts
@@ -41,23 +41,13 @@ export async function runMigrations(db: Db): Promise<void> {
 
     await db.executeSql(
       `
-      CREATE INDEX workflow_runs_workflow_id_idx ON workflow_runs USING btree (workflow_id);
-    `,
-      [],
-    );
-
-    await db.executeSql(
-      `
       CREATE INDEX workflow_runs_created_at_idx ON workflow_runs USING btree (created_at);
+      CREATE INDEX workflow_runs_resource_id_created_at_idx ON workflow_runs USING btree (resource_id, created_at DESC);
+      CREATE INDEX workflow_runs_status_created_at_idx ON workflow_runs USING btree (status, created_at DESC);
+      CREATE INDEX workflow_runs_workflow_id_created_at_idx ON workflow_runs USING btree (workflow_id, created_at DESC);
+      CREATE INDEX workflow_runs_resource_id_workflow_id_created_at_idx ON workflow_runs USING btree (resource_id, workflow_id, created_at DESC);
     `,
       [],
-    );
-
-    await db.executeSql(
-      `
-      CREATE INDEX workflow_runs_resource_id_idx ON workflow_runs USING btree (resource_id);
-    `,
-      [],
-    );
+    )
   }
 }
